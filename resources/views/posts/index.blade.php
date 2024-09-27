01f0f44a269bd52e4e0835894b3c37051f295129
samraak <samraazhar57@gmail.com>
2024-09-27 02:42:44 -0700

    Blogpost Project

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,6 +1,12 @@
-
-
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
 <div class="container">
     <h1>Blog Posts</h1>
     <table class="table">
@@ -31,4 +37,8 @@
         </tbody>
     </table>
 </div>
+</body>
+</html>
+
+
 
